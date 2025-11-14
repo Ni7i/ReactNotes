2f6ea7f2cb04059222d15f278ec5aa55f35552b1
Ni7i <enisshorra@Eniss-MacBook-Pro.local>
2025-11-14 10:40:25 +0100

    Aufbau der Klasse Message.tsx und ListGroup.tsx

---
+++ ReactNotes/src/Components/ListGroup.tsx
@@ -0,0 +1,21 @@
+function ListGroup() {
+
+    let items = [
+        "Fatlum",
+        "Dion",
+        "Ylli",
+        "Sami"
+    ];
+    items = [];
+
+    return (
+        <>
+            <h1>List</h1>
+            <ul className="list-group">
+                {items.map(item => <li key={item}>{item}</li>)}
+            </ul>
+        </>
+    );
+}
+
+export default ListGroup;
---
+++ ReactNotes/src/Message.tsx
@@ -0,0 +1,17 @@
+//React uses PascalCase
+
+function Message() {
+
+    const name = "Nisi"
+
+    if (name == "Nisi")
+
+        return <h1>Hello {name}</h1>
+
+    else (name == "Nisi")
+    return <h1>your not the admin!</h1>
+
+
+}
+
+export default Message;
